09eca6b1d253187090c0973d09e8b8e6c3917504
sSwiergosz <szymon.swiergosz@gmail.com>
2018-03-30 18:02:23 +0200

    Almost

--- nbp2.py
+++ nbp2.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from hashlib import md5
 import sys, json
 import requests
+import re
 
 # TODO 
 # 2017: w niektorych brak contentu, dodajace sie skrypty jquery do contentu, niezamkniety tag w 'moneta roku 2017'
@@ -16,87 +17,115 @@ import requests
 simple_json = {}
 l = []
 #year = sys.argv[1]
-year = 2014
+# year = 2002
 
-index = urllib.request.urlopen('http://www.nbp.pl/home.aspx?f=/aktualnosci/wiadomosci_{0}.htm'.format(year))
-index_soup = BeautifulSoup(index, 'lxml')
-anchors = index_soup.find_all('a', class_='news_head')
-dates = index_soup.find_all('span', class_='date')
-titles = index_soup.find_all('a', class_='news_head')
+for year in range (1998, 2005):
 
-for a in range(0, len(anchors)):
-    content = ''
-    
-    if anchors[a].get('href').startswith('/home.aspx'):
-        # ignoring non-ascii characters in link
-        href = anchors[a].get('href').encode('utf-8').decode('ascii', 'ignore')
-        url = 'http://www.nbp.pl{0}'.format(href)
-        request = requests.get(url)
-        if not request.status_code == 200: # if site doesn't exist go to another one
-            continue
+    index = urllib.request.urlopen('http://www.nbp.pl/home.aspx?f=/aktualnosci/wiadomosci_{0}.htm'.format(year))
+    index_soup = BeautifulSoup(index, 'lxml')
+    anchors = index_soup.find_all('a', class_='news_head')
+    dates = index_soup.find_all('span', class_='date')
+    titles = index_soup.find_all('a', class_='news_head')
 
-        # print(url)
-        statement = urllib.request.urlopen(url)
-        statement_soup = BeautifulSoup(statement, 'lxml')
+    for a in range(0, len(anchors)):
+        content = ''
 
-        iid = int(md5(url.encode('utf-8')).hexdigest(), 16)
-        simple_json["iid"] = iid
-        simple_json["id"] = '{}-1'.format(iid)
-        simple_json["url"] = url
-        simple_json["lname"] = "nbp.pl"
+        if anchors[a].get('href').startswith('/home.aspx'):
+            # ignoring non-ascii characters in link
+            href = anchors[a].get('href').encode('utf-8').decode('ascii', 'ignore')
+            # url = 'http://www.nbp.pl{0}'.format(href)
+            url = 'http://www.nbp.pl/home.aspx?f=aktualnosci/wiadomosci_2004/rpp_250804_rpp.html'
+            request = requests.get(url)
+            if not request.status_code == 200: # if site doesn't exist go to another one
+                continue
 
-        # there are two different types of date sep: 2018-2004 is '-', older years is '.'
-        time = dates[a].text[1:-1]
-        if time[-5] == '.':
-            time = dates[a].text[1:-1].split('.')
-        elif time[-5] == '-':
-            time = dates[a].text[1:-1].split('-')
+            statement = urllib.request.urlopen(url)
+            statement_soup = BeautifulSoup(statement, 'lxml')
 
-        simple_json["title"] = titles[a].text
-        simple_json["duration_start"] = "{}-{}-{}T00:00:00Z".format(time[2], time[1], time[0])
-        simple_json["duration_end"] = "{}-{}-{}T23:59:59Z".format(time[2], time[1], time[0])
+            iid = int(md5(url.encode('utf-8')).hexdigest(), 16)
+            simple_json["iid"] = iid
+            simple_json["id"] = '{}-1'.format(iid)
+            simple_json["url"] = url
+            simple_json["lname"] = "nbp.pl"
 
-        # there are different types of building pages across the years
-        if statement_soup.find('div', id='contentholder'):
-            contentholder = statement_soup.find('div', id='contentholder')
-            paragraphs = contentholder.find_all('p', class_=None)
+            # there are two different types of date sep: 2018-2004 is '-', older years is '.'
+            time = dates[a].text[1:-1]
+            if time[-5] == '.':
+                time = dates[a].text[1:-1].split('.')
+            elif time[-5] == '-':
+                time = dates[a].text[1:-1].split('-')
 
-            # not all statements have class 'srodtytul'
-            if contentholder.find('p', class_='srodtytul'):
-                content += contentholder.find('p', class_='srodtytul').text.strip()
-                content += '\n'
+            simple_json["title"] = re.sub('\s+', ' ', titles[a].text)
+            simple_json["duration_start"] = "{}-{}-{}T00:00:00Z".format(time[2], time[1], time[0])
+            simple_json["duration_end"] = "{}-{}-{}T23:59:59Z".format(time[2], time[1], time[0])
+
+            # there are different types of building pages across the years
+            if statement_soup.find('div', id='contentholder'):
+                contentholder = statement_soup.find('div', id='contentholder')
+                paragraphs = contentholder.find_all('p', class_=None)
+
+                # not all statements have class 'srodtytul'
+                if contentholder.find('p', class_='srodtytul'):
+                    srod = contentholder.find('p', class_='srodtytul').text
+                    content += re.sub('\s+', ' ', srod)
+                    content += '\n'
+                
+                if contentholder.find_all('li'):
+                    for c in contentholder.find_all('li'):
+                        content += re.sub('\s+', ' ', c.text)
+                    content += '\n'
+                
+                for p in paragraphs:
+                    content += re.sub('\s+', ' ', p.text)
+                    content += '\n'
             
-            if contentholder.find_all('li'):
-                for c in contentholder.find_all('li'):
-                    content += c.text
-                content += 'n'
-            
-            for p in paragraphs:
-                content += p.text.strip()
-                content += '\n'
-        
-        else:
-            if len(statement_soup.find('div', id='article').find_all('td')) != 1:
-                td = statement_soup.find('div', id='article').find_all('td')[1]
             else:
-                td = statement_soup.find('div', id='article').find('td')
-            if td.find('p'):
-                if td.find_all('li'): # if there are lists
-                    for i in td.find_all('p'):
-                        l.append(i)
-                    for i in td.find_all('li'):
-                        l.append(i)
+                td_list = statement_soup.find('div', id='article').find_all('td')
+                if len(td_list) != 1:
+                    if td_list[0].find('p', class_='srodtytul'):
+                        td = td_list[0]
+                    else:
+                        if 'img' not in str(td_list):
+                            td = td_list[1]
+                        else:
+                            td = td_list[0]
                 else:
-                    for i in td.find_all('p'):
-                        l.append(i)
-                for i in l:
-                    content += i.text
-                    content += '\n'
-            elif td.find_all('div', class_='justify'):
-                for i in td.find_all('div', class_='justify'):
-                    content += i.text.strip()
-                    content += '\n'
+                    td = td_list[0]
 
-        simple_json['content'] = content
+                if td.find('p'):
+                    if td.find_all('li'): # if there are li's
+                        for i in td.find_all('p'):
+                            if not i.text.startswith(' Data'):
+                                if i.get('class') != 'head1' or i.get('class') != 'head2':
+                                    l.append(i)
+                        for i in td.find_all('li'):
+                            l.append(i)
+                    else:
+                        for i in td.find_all('p'):
+                            if not i.text.startswith(' Data'):
+                                if i.get('class') != None:
+                                    if i.get('class')[0] != 'head1' or i.get('class') != 'head2':
+                                        l.append(i)
+                                else:
+                                    l.append(i)
+                    for i in l:
+                        content += re.sub('\s+', ' ', i.text)
+                        content += '\n'
 
-    print(simple_json)
+                elif td.find_all('div', class_='justify'):
+                    for i in td.find_all('div', class_='justify'):
+                        content += re.sub('\s+', ' ', i.text)
+                        content += '\n'
+                elif td.find('div', class_='just'):
+                    for i in td.find_all('div', class_='just'):
+                        content += re.sub('\s+', ' ', i.text)
+                        content += '\n'
+                elif td.find('ol'):
+                    if td.find('ol').find('li'):
+                        for i in td.find_all('li'):
+                            content += re.sub('\s+', ' ', i.text)
+                            content += '\n'
+
+            simple_json['content'] = content
+
+        print(simple_json)
